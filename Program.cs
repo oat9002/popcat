df6156be72c37bf0f67eb94cf1b06a8365b287c7
oat9002 <oat9002@hotmail.com>
2022-03-06 23:56:43 +0700

    :recycle:

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+
+namespace Pop.Cat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver();
+
+            try
+            {
+                driver.Navigate().GoToUrl("https://popcat.click/");
+
+                var cat = driver.FindElementByClassName("cat-img");
+
+                for (var i = 0; i < 1000; i++)
+                {
+                    cat.Click();
+                    Thread.Sleep(200);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            driver.Close();
+        }
+    }
+}
